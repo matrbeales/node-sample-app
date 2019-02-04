235f9062b5d4ef9f39cee28a1b75635943a587c0
tech-w129b <mbeales@spartaglobal.com>
2019-02-04 12:11:19 +0000

    Initial commit

---
+++ environment/app/provision.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Update the sources list
+sudo apt-get update -y
+
+# upgrade any packages available
+sudo apt-get upgrade -y
+
+# install nginx
+sudo apt-get install nginx -y
+
+# install git
+sudo apt-get install git -y
+
+# install nodejs
+sudo apt-get install python-software-properties
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install nodejs -y
+
+# install pm2
+sudo npm install pm2 -g
+
+sudo rm /etc/nginx/nginx.conf
+sudo ln -s /home/ubuntu/environment/app/nginx.conf /etc/nginx/nginx.conf
+
+sudo systemctl restart nginx
+sudo systemctl enable nginx 
